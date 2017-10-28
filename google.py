e9ea05256de2a483fb3c29a481265840b01bca6f
Angelo Gazzola <angelo@nglgzz.com>
2017-10-28 13:24:06 +0200

    added descriptions on results

--- google.py
+++ google.py
@@ -17,6 +17,7 @@ __dependencies__ = ['lxml', 'cssselect']
 __icon__ = path.dirname(__file__) + '/icons/Google.png'
 
 
+MAX_LINE_LENGTH = 75
 REQUEST_HEADERS = {
   'User-Agent': (
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
@@ -64,11 +65,23 @@ def search(query):
       'q': query,
     }
   )
-  get_url = lambda el: el.get('href')
   html = lxml.html.fromstring(response.text)
-  results = html.cssselect('h3 > a')
-  results = [(r.text_content(), get_url(r)) for r in results]
-  return [ResultItem(r) for r in results]
+  results = html.cssselect('.rc')
+  items = []
+
+  for result in results:
+    title = result.cssselect('h3 > a')[0]
+    url = title.get('href')
+    description = result.cssselect('.s .st')[0].text_content()
+    formatted_description = []
+
+    for i in range(MAX_LINE_LENGTH, len(description), MAX_LINE_LENGTH):
+      formatted_description.append(description[i-MAX_LINE_LENGTH:i])
+    formatted_description.append(description[i:-1])
+
+    items.append((title.text, url + '\n' + '\n'.join(formatted_description)))
+
+  return [ResultItem(r) for r in items]
 
 
 def suggest(query):
